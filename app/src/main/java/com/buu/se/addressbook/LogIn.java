9cf90210040ceb0961496e12d89a0808a793c1ce
Thamrong Sunate <thamrong.sunate@gmail.com>
2015-05-08 16:30:11 +0700

    Initial commit

---
+++ app/src/main/java/com/buu/se/addressbook/LogIn.java
@@ -0,0 +1,48 @@
+package com.buu.se.addressbook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class LogIn extends Activity {
+
+    final int MYACTIVITY_REQUEST_CODE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_log_in, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickLogin(View v){
+        Intent data = new Intent(LogIn.this, MainActivity.class);
+        startActivityForResult(data, MYACTIVITY_REQUEST_CODE);
+    }
+}
